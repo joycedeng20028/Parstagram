802783dd76d08347a611fac858317c3b4a025511
Joyce Deng <joycedeng@fb.com>
2021-07-06 16:25:51 -0700

    Added logout

--- app/src/main/java/com/example/parstagram/LoginActivity.java
+++ app/src/main/java/com/example/parstagram/LoginActivity.java
@@ -87,7 +87,7 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 
-    private void goMainActivity() {
+    public void goMainActivity() {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
         finish();
